5c16da4ecd3c33f3b9b809e37bd2104094955238
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-22 13:38:38 -0300

    Criação das migrations, model, e rotas, usando sintaxe: php artisan make:model -mcr {Nome do model}, fazendo a configuracao de conexao do banco no arquivo .env, subindo as migrations no banco.

---
+++ database/migrations/2024_11_21_193535_create_carros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carros', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('modelo_id');
+            $table->foreign('modelo_id')->references('id')->on('modelos');
+            $table->string('placa',10)->unique();
+            $table->boolean('disponivel');
+            $table->integer('km');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('carros');
+        Schema::enableForeignKeyConstraints();
+    }
+}
