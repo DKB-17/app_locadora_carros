6912db3dd43cb3896f76f840767bca8ad110bdb2
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-27 16:15:56 -0300

    Criando um Repository Design Patter, para implementar as regras de negocio do sistema de forma a reutilizar codigo.

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Marca;
+use App\Repositories\MarcaRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Ramsey\Uuid\Type\Integer;
@@ -21,35 +22,25 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-        $marcas = array();
+
+        $marcaRepository = new MarcaRepository($this->marca);
 
         if($request->has('atributos_modelos')){
-            $atributos_modelos = $request->atributos_modelos;
-            $marcas = $this->marca->with('modelos:id,'.$atributos_modelos);
+            $atributos_modelos = 'modelos:id,'.$request->atributos_modelos;
+            $marcaRepository->selectAtributosRegistrosRelacionados($atributos_modelos);
         }else{
-            $marcas = $this->marca->with('modelos');
+            $marcaRepository->selectAtributosRegistrosRelacionados('modelos');
         }
 
         if($request->has('filtro')){
-            $filtros = explode(';', $request->filtro);
-            foreach($filtros as $key => $condicao){
-                $c = explode(':', $condicao); // : usado para separa (coluna):(operador):(condicao)
-                $marcas = $marcas->where($c[0], $c[1], $c[2]);
-            }
+            $marcaRepository->filtro($request->filtro);
         }
 
         if($request->has('atributos')){
-
-            $atributos = $request->atributos;
-            $marcas = $marcas->selectRaw($atributos)->get();
-
-        }else{
-            $marcas = $marcas->get();
+            $marcaRepository->selectAtributos($request->atributos);
         }
 
-        dd($marcas);
-
-        return response()->json($marcas, 200);
+        return response()->json($marcaRepository->getResultado(), 200);
 
 
         //$marcas = Marca::all();
