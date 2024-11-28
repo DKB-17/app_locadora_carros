fe8e7dc04efbc0afa183ed0e7b3e41daa5c24e4d
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-28 13:14:21 -0300

    Agrupando as rota para serem autenticadas pelo middleware

--- routes/api.php
+++ routes/api.php
@@ -19,11 +19,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //Route::resource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('cliente', 'ClienteController');
-Route::apiResource('carro', 'CarroController');
-Route::apiResource('locacao', 'LocacaoController');
-Route::apiResource('marca', 'MarcaController');
-Route::apiResource('modelo', 'ModeloController');
+Route::middleware('jwt.auth')->group(function () {
+    Route::apiResource('cliente', 'ClienteController');
+    Route::apiResource('carro', 'CarroController');
+    Route::apiResource('locacao', 'LocacaoController');
+    Route::apiResource('marca', 'MarcaController');
+    Route::apiResource('modelo', 'ModeloController');
+});
+
 
 Route::post('login', 'AuthController@login');
 Route::post('logout', 'AuthController@logout');
