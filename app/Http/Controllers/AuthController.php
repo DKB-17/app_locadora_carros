6deceed851e7ef9c90927fa1b0412804197e1ae0
diego <136401880+DKB-17@users.noreply.github.com>
2024-11-28 11:23:26 -0300

    Implementando o metodo de login(autenticacao), retorno do token de validacao de usuario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,13 +6,29 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function __conctruct()
-    {
 
-    }
-
-    public function login()
+    public function login(Request $request)
     {
+        $credenciais = $request->all(['email', 'password']);
+
+        //autenticacao (email e senha)
+        $token = auth('api')->attempt($credenciais);
+
+        /*
+         * Caso exista as credenciais estaja valida o token e retornado
+         * Caso não e retornado 'false'
+         * */
+
+        if($token){ //usuari autenticado com sucesso
+            return response()->json(['token' => $token],200);
+        }else{ // erro de usuario ou senha
+            return response()->json(['error' => 'Usuário ou senha invalido!'],403);
+            // 401 = Unauthorized -> nao autorizado
+            // 403 = Forbidden -> proibido (login invalido)
+        }
+
+        //retornar um Json Web Token
+
         return 'login';
     }
 
